4ce1548dbbed347f5f11c281ff59bb8a8807848f
Yanzong Chan <3267189544@qq.com>
2026-05-17 15:05:50 +0800

    configure custom domain

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig( {
-  base: '/bolg-notes/',
+  base: '/',
   title: 'Jinfeng Notes',
   description: 'Personal technical notes and essays',
 
